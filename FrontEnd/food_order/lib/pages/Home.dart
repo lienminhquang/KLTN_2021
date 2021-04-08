c987950c66cc2b6da570d1cf21b0e558d3008f20
gameloft <gameloft@Vam-MAC.sai.gameloft.org>
2021-04-08 11:06:16 +0700

    + add BottomNavigationBar

--- FrontEnd/food_order/lib/pages/Home.dart
+++ FrontEnd/food_order/lib/pages/Home.dart
@@ -31,6 +31,12 @@ class _MyHomePageState extends State<MyHomePage> {
       dotSpacing: 10.0,
     ),
   );
+  int _selectedIndex = 0;
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -138,6 +144,38 @@ class _MyHomePageState extends State<MyHomePage> {
             FastChoice()
           ],
         ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+            backgroundColor: Colors.red,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.article_rounded),
+            label: 'Orders',
+            backgroundColor: Colors.green,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite_rounded),
+            label: 'Favorite',
+            backgroundColor: Colors.purple,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.notifications_rounded),
+            label: 'Notification',
+            backgroundColor: Colors.pink,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_box_rounded),
+            label: 'Profile',
+            backgroundColor: Colors.yellow,
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
       ),
     );
   }
