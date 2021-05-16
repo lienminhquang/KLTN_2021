bec5de16f41234107784b5f5c29cd384628d74c2
lmq <minhquang130698@gmail.com>
2021-05-16 14:45:50 +0700

    ! Allow only bad request from our domain.

--- FrontEnd/food_order/lib/configs/DevHttpsOveride.dart
+++ FrontEnd/food_order/lib/configs/DevHttpsOveride.dart
@@ -4,7 +4,11 @@ class DevHttpOverrides extends HttpOverrides {
   @override
   HttpClient createHttpClient(SecurityContext? context) {
     return super.createHttpClient(context)
-      ..badCertificateCallback =
-          (X509Certificate cert, String host, int port) => true;
+      ..badCertificateCallback = (X509Certificate cert, String host, int port) {
+        if (host == "localhost" && port == 6001) {
+          return true;
+        }
+        return false;
+      };
   }
 }
