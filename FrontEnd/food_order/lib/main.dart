7a9b5467409e09bbefd261039ee62c80f51bc842
lmq <minhquang130698@gmail.com>
2021-05-16 14:38:26 +0700

    ! Accept all bad certificate request for dev.

--- FrontEnd/food_order/lib/main.dart
+++ FrontEnd/food_order/lib/main.dart
@@ -1,11 +1,19 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:food_delivery/configs/DevHttpsOveride.dart';
+import 'package:food_delivery/models/CategoryModel.dart';
 // import 'package:food_delivery/pages/MainPages.dart';
 // import 'package:food_delivery/pages/Home.dart';
 import 'package:food_delivery/pages/login_signup/Login.dart';
 
 import 'package:food_delivery/routers.dart';
+import 'package:provider/provider.dart';
 
 void main() {
+  //acept bad certificate request
+  HttpOverrides.global = new DevHttpOverrides();
+
   runApp(MyApp());
 }
 
@@ -13,15 +21,20 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Food Delivery',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => CategoryModel()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Food Delivery',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        routes: routes,
+        initialRoute: LoginPage.routeName,
       ),
-      routes: routes,
-      initialRoute: LoginPage.routeName,
     );
   }
 }
--- FrontEnd/food_order/lib/services/HttpClientFactory.dart
+++ FrontEnd/food_order/lib/services/HttpClientFactory.dart
@@ -4,11 +4,11 @@ import 'package:http/io_client.dart';
 
 class HttpClientFactory {
   IOClient createIOClient() {
-    HttpClient httpClient = new HttpClient()
-      ..badCertificateCallback =
-          ((X509Certificate cert, String host, int port) {
-        return true;
-      });
+    HttpClient httpClient = new HttpClient();
+    // ..badCertificateCallback =
+    //     ((X509Certificate cert, String host, int port) {
+    //   return true;
+    // });
     return new IOClient(httpClient);
   }
 }
